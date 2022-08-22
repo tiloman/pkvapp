9d209394677308098327a6d98ba65069cc7eb3fe
Timo Lohmann <47814142+tiloman@users.noreply.github.com>
2022-08-22 20:28:36 +0200

    update ruby

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -23,12 +23,10 @@
 //= require best_in_place
 //= require best_in_place.jquery-ui
 
-
 //= require jquery.purr
 //= require best_in_place.purr
 //= require select2
 //= require select2_locale_de
-
 
 //= require moment
 //= require fullcalendar
@@ -36,127 +34,121 @@
 
 //= require_tree .
 
-
-
 function operationsCalendar() {
   var options = {
-    events: '/operations.json',
-    eventColor: '#016973',
-    themeSystem: 'standard',
-    defaultView: 'month',
-    locale: 'de',
+    events: "/operations.json",
+    eventColor: "#2c3e50",
+    themeSystem: "standard",
+    defaultView: "month",
+    locale: "de",
     fixedWeekCount: true,
 
     header: {
-      left: 'prev,next today',
-      center: 'title',
-      right: 'month,listWeek'
-      }
-     };
+      left: "prev,next today",
+      center: "title",
+      right: "month,listWeek",
+    },
+  };
 
-  return $('#calendar').fullCalendar(options);
-};
+  return $("#calendar").fullCalendar(options);
+}
 
 function clearCalendar() {
-  $('#calendar').fullCalendar('delete');
-  $('#calendar').html('');
-};
+  $("#calendar").fullCalendar("delete");
+  $("#calendar").html("");
+}
 
-$(document).on('turbolinks:load', operationsCalendar);
-$(document).on('turbolinks:before-cache', clearCalendar)
+$(document).on("turbolinks:load", operationsCalendar);
+$(document).on("turbolinks:before-cache", clearCalendar);
 
-
-document.addEventListener("turbolinks:load", function() {
-
+document.addEventListener("turbolinks:load", function () {
   /* Activating Best In Place */
   $(".best_in_place").best_in_place();
 
+  //   $("input.datepicker").each(function(input) {
+  //   $(this).datepicker({
+  //     dateFormat: "dd.mm.yy",
+  //     altField: $(this).next()
+  //   })
+  //
+  //
+  //   // If you use i18n-js you can set the locale like that
+  //   //$(this).datepicker("option", $.datepicker.regional[I18n.currentLocale()]);
+  // })
 
-//   $("input.datepicker").each(function(input) {
-//   $(this).datepicker({
-//     dateFormat: "dd.mm.yy",
-//     altField: $(this).next()
-//   })
-//
-//
-//   // If you use i18n-js you can set the locale like that
-//   //$(this).datepicker("option", $.datepicker.regional[I18n.currentLocale()]);
-// })
-
-$( ".select2_form" ).select2({
-    theme: "bootstrap"
+  $(".select2_form").select2({
+    theme: "bootstrap",
+  });
 });
 
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-document.addEventListener("turbolinks:load", function() {
-  $("body").on("change", ".ajax-input", function() {
+document.addEventListener("turbolinks:load", function () {
+  $("body").on("change", ".ajax-input", function () {
     Rails.fire(this.form, "submit");
   });
 
-
-
-  $('.best_in_place').bind("ajax:success", function () {
-    $(this).closest('tr').effect('highlight');
+  $(".best_in_place").bind("ajax:success", function () {
+    $(this).closest("tr").effect("highlight");
     update_values();
-    }
-  );
-
-  function update_values(){
-    var operation_id = window.location.pathname.split('/')[2];
-    var difference = $('#insurance_difference').html();
-    difference = parseInt(difference.replace(/,/g,''))
-
-    var insurance_ratio = $('#insurance_ratio').html();
-    var insurance_payback = $('#best_in_place_operation_'+ operation_id + '_insurance_payback').html();
-    insurance_payback = parseInt(insurance_payback.replace(/,/g,''))
-    var assistance_ratio = $('#assistance_ratio').html();
-    var assistance_payback = $('#best_in_place_operation_'+ operation_id + '_assistance_payback').html();
-    assistance_payback = parseInt(assistance_payback.replace(/,/g,''))
-    var value = $('#best_in_place_operation_'+ operation_id + '_value').html();
-    value = parseInt(value.replace(/,/g,''))
-
-
-    $('#insurance_difference').html((Math.round((insurance_payback - (value * (insurance_ratio)))/100 * 100) / 100).toFixed(2));
-    $('#insurance_calculated').html((Math.round(value * (insurance_ratio/100)).toFixed(2)));
-    $('#assistance_difference').html((Math.round((assistance_payback - (value * (assistance_ratio)))/100 * 100) / 100).toFixed(2));
-    $('#assistance_calculated').html((Math.round(value * (assistance_ratio/100)).toFixed(2)));
-  }
-
-
-  function numberWithCommas(x) {
-      return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-  }
-
-
-
-  $('#uploadForm input').change(function(){
-   $(this).parent().ajaxSubmit({
-    beforeSubmit: function(a,f,o) {
-     o.dataType = 'json';
-    },
-    complete: function(XMLHttpRequest, textStatus) {
-     // XMLHttpRequest.responseText will contain the URL of the uploaded image.
-     // Put it in an image element you create, or do with it what you will.
-     // For example, if you have an image elemtn with id "my_image", then
-     $('#bill').attr('src', XMLHttpRequest.responseText);
-     // Will set that image tag to display the uploaded image.
-    },
-   });
   });
 
+  function update_values() {
+    var operation_id = window.location.pathname.split("/")[2];
+    var difference = $("#insurance_difference").html();
+    difference = parseInt(difference.replace(/,/g, ""));
 
+    var insurance_ratio = $("#insurance_ratio").html();
+    var insurance_payback = $(
+      "#best_in_place_operation_" + operation_id + "_insurance_payback"
+    ).html();
+    insurance_payback = parseInt(insurance_payback.replace(/,/g, ""));
+    var assistance_ratio = $("#assistance_ratio").html();
+    var assistance_payback = $(
+      "#best_in_place_operation_" + operation_id + "_assistance_payback"
+    ).html();
+    assistance_payback = parseInt(assistance_payback.replace(/,/g, ""));
+    var value = $("#best_in_place_operation_" + operation_id + "_value").html();
+    value = parseInt(value.replace(/,/g, ""));
 
+    $("#insurance_difference").html(
+      (
+        Math.round(
+          ((insurance_payback - value * insurance_ratio) / 100) * 100
+        ) / 100
+      ).toFixed(2)
+    );
+    $("#insurance_calculated").html(
+      Math.round(value * (insurance_ratio / 100)).toFixed(2)
+    );
+    $("#assistance_difference").html(
+      (
+        Math.round(
+          ((assistance_payback - value * assistance_ratio) / 100) * 100
+        ) / 100
+      ).toFixed(2)
+    );
+    $("#assistance_calculated").html(
+      Math.round(value * (assistance_ratio / 100)).toFixed(2)
+    );
+  }
+
+  function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+  $("#uploadForm input").change(function () {
+    $(this)
+      .parent()
+      .ajaxSubmit({
+        beforeSubmit: function (a, f, o) {
+          o.dataType = "json";
+        },
+        complete: function (XMLHttpRequest, textStatus) {
+          // XMLHttpRequest.responseText will contain the URL of the uploaded image.
+          // Put it in an image element you create, or do with it what you will.
+          // For example, if you have an image elemtn with id "my_image", then
+          $("#bill").attr("src", XMLHttpRequest.responseText);
+          // Will set that image tag to display the uploaded image.
+        },
+      });
+  });
 });
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,19 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
   after_create :create_person
-
   has_many :people, dependent: :destroy
 
   def create_person
-	   Person.create(name: self.first_name, color: "red", ratio: "50/50")
+    Person.create(name: first_name, color: 'red', ratio: '50/50')
   end
 
   def full_name
     if first_name.present? && last_name.present?
-      return first_name + " " + last_name
+      first_name + ' ' + last_name
     else
-      return email
+      email
     end
   end
-  
 end
