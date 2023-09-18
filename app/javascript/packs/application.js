6b19dd04bf0277c8f4d933b76ad2c414cdb94de0
Timo Lohmann <47814142+tiloman@users.noreply.github.com>
2023-09-18 07:56:13 +0200

    update environment js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,8 +4,9 @@ require('turbolinks').start()
 require("trix")
 require("@rails/actiontext")
 window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js')
+import 'bootstrap';
 
 import '@fortawesome/fontawesome-free/css/all'
-import "controllers"
 
-require("../stylesheets/application.scss")
+import "../stylesheets/application"
+import "controllers"
